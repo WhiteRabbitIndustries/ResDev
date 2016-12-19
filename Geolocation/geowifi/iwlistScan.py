44ae782ffb51de9fb5111cc23af194d7782f2595
Imanol Gomez <yo@imanolgomez.net>
2016-12-19 10:54:58 +0100

    iwlist command adapted to chip
    Using /sbin/iwlist instead

--- Geolocation/geowifi/iwlistScan.py
+++ Geolocation/geowifi/iwlistScan.py
@@ -22,6 +22,7 @@ def get_quality(cell):
     return str(int(round(float(quality[0]) / float(quality[1]) * 100))).rjust(3) + " %"
 
 def get_channel(cell):
+    return matching_line(cell,"Channel:")
 
 def get_frequency(cell):
     frequency =  matching_line(cell,"Frequency:").split(" ")[0]
@@ -153,7 +154,7 @@ def main():
     cells=[[]]
     parsed_cells=[]
 
-    proc = subprocess.Popen(["iwlist", interface, "scan"],stdout=subprocess.PIPE, universal_newlines=True)
+    proc = subprocess.Popen(["/sbin/iwlist", interface, "scan"],stdout=subprocess.PIPE, universal_newlines=True)
     out, err = proc.communicate()
 
     for line in out.split("\n"):
